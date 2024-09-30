aa30b9b82970458a0633edf34e41d24391535129
Siméon DAOUDA <simeondaouda@gmail.com>
2024-09-30 17:02:28 +0100

    first commit : init

---
+++ android/app/src/main/kotlin/com/example/aventter_shop_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.aventter_shop_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/gen/assets.gen.dart
@@ -0,0 +1,160 @@
+/// GENERATED CODE - DO NOT MODIFY BY HAND
+/// *****************************************************
+///  FlutterGen
+/// *****************************************************
+
+// coverage:ignore-file
+// ignore_for_file: type=lint
+// ignore_for_file: directives_ordering,unnecessary_import,implicit_dynamic_list_literal,deprecated_member_use
+
+import 'package:flutter/widgets.dart';
+
+class $AssetsImagesGen {
+  const $AssetsImagesGen();
+
+  /// File path: assets/images/africa.jpg
+  AssetGenImage get africa => const AssetGenImage('assets/images/africa.jpg');
+
+  /// File path: assets/images/badgirl.jpg
+  AssetGenImage get badgirl => const AssetGenImage('assets/images/badgirl.jpg');
+
+  /// File path: assets/images/chemise.jpg
+  AssetGenImage get chemise => const AssetGenImage('assets/images/chemise.jpg');
+
+  /// File path: assets/images/dress.jpg
+  AssetGenImage get dress => const AssetGenImage('assets/images/dress.jpg');
+
+  /// File path: assets/images/dress0.jpg
+  AssetGenImage get dress0 => const AssetGenImage('assets/images/dress0.jpg');
+
+  /// File path: assets/images/dress2.webp
+  AssetGenImage get dress2 => const AssetGenImage('assets/images/dress2.webp');
+
+  /// File path: assets/images/dress3.jpg
+  AssetGenImage get dress3 => const AssetGenImage('assets/images/dress3.jpg');
+
+  /// File path: assets/images/dress4.jpg
+  AssetGenImage get dress4 => const AssetGenImage('assets/images/dress4.jpg');
+
+  /// File path: assets/images/man.jpg
+  AssetGenImage get man => const AssetGenImage('assets/images/man.jpg');
+
+  /// File path: assets/images/red_dress.jpg
+  AssetGenImage get redDress =>
+      const AssetGenImage('assets/images/red_dress.jpg');
+
+  /// File path: assets/images/red_dress_2.jpg
+  AssetGenImage get redDress2 =>
+      const AssetGenImage('assets/images/red_dress_2.jpg');
+
+  /// File path: assets/images/survetement.webp
+  AssetGenImage get survetement =>
+      const AssetGenImage('assets/images/survetement.webp');
+
+  /// File path: assets/images/women.jpg
+  AssetGenImage get women => const AssetGenImage('assets/images/women.jpg');
+
+  /// List of all assets
+  List<AssetGenImage> get values => [
+        africa,
+        badgirl,
+        chemise,
+        dress,
+        dress0,
+        dress2,
+        dress3,
+        dress4,
+        man,
+        redDress,
+        redDress2,
+        survetement,
+        women
+      ];
+}
+
+class Assets {
+  Assets._();
+
+  static const $AssetsImagesGen images = $AssetsImagesGen();
+}
+
+class AssetGenImage {
+  const AssetGenImage(
+    this._assetName, {
+    this.size,
+    this.flavors = const {},
+  });
+
+  final String _assetName;
+
+  final Size? size;
+  final Set<String> flavors;
+
+  Image image({
+    Key? key,
+    AssetBundle? bundle,
+    ImageFrameBuilder? frameBuilder,
+    ImageErrorWidgetBuilder? errorBuilder,
+    String? semanticLabel,
+    bool excludeFromSemantics = false,
+    double? scale,
+    double? width,
+    double? height,
+    Color? color,
+    Animation<double>? opacity,
+    BlendMode? colorBlendMode,
+    BoxFit? fit,
+    AlignmentGeometry alignment = Alignment.center,
+    ImageRepeat repeat = ImageRepeat.noRepeat,
+    Rect? centerSlice,
+    bool matchTextDirection = false,
+    bool gaplessPlayback = false,
+    bool isAntiAlias = false,
+    String? package,
+    FilterQuality filterQuality = FilterQuality.low,
+    int? cacheWidth,
+    int? cacheHeight,
+  }) {
+    return Image.asset(
+      _assetName,
+      key: key,
+      bundle: bundle,
+      frameBuilder: frameBuilder,
+      errorBuilder: errorBuilder,
+      semanticLabel: semanticLabel,
+      excludeFromSemantics: excludeFromSemantics,
+      scale: scale,
+      width: width,
+      height: height,
+      color: color,
+      opacity: opacity,
+      colorBlendMode: colorBlendMode,
+      fit: fit,
+      alignment: alignment,
+      repeat: repeat,
+      centerSlice: centerSlice,
+      matchTextDirection: matchTextDirection,
+      gaplessPlayback: gaplessPlayback,
+      isAntiAlias: isAntiAlias,
+      package: package,
+      filterQuality: filterQuality,
+      cacheWidth: cacheWidth,
+      cacheHeight: cacheHeight,
+    );
+  }
+
+  ImageProvider provider({
+    AssetBundle? bundle,
+    String? package,
+  }) {
+    return AssetImage(
+      _assetName,
+      bundle: bundle,
+      package: package,
+    );
+  }
+
+  String get path => _assetName;
+
+  String get keyName => _assetName;
+}
